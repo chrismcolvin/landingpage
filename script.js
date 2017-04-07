aa9e96ac1901a6d44bbacea98d5a077f8e26c251
Chris Colvin <chrismcolvin@hotmail.com>
2017-04-06 21:14:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,88 @@
+/* JS used from https://codepen.io/jacmaes/pen/GFDEK */
+
+window.onload = function (){
+
+	document.documentElement.className = 'js';
+
+var menu = document.querySelector('.menu'),
+	button = document.querySelector('.toggle'),
+	dropdown = document.querySelector('.burger');
+
+button.onclick = function() {
+	classie.toggle(menu, 'menu-active');
+};
+
+//Close menu when clicking outside
+document.addEventListener('click', function(event) {
+	if (event.target !== dropdown && !dropdown.contains(event.target)) {
+		classie.removeClass(menu, 'menu-active');
+	}
+});
+};
+
+( function( window ) {
+
+'use strict';
+
+function classReg( className ) {
+  return new RegExp("(^|\\s+)" + className + "(\\s+|$)");
+}
+
+var hasClass, addClass, removeClass;
+
+if ( 'classList' in document.documentElement ) {
+  hasClass = function( elem, c ) {
+    return elem.classList.contains( c );
+  };
+  addClass = function( elem, c ) {
+    elem.classList.add( c );
+  };
+  removeClass = function( elem, c ) {
+    elem.classList.remove( c );
+  };
+}
+else {
+  hasClass = function( elem, c ) {
+    return classReg( c ).test( elem.className );
+  };
+  addClass = function( elem, c ) {
+    if ( !hasClass( elem, c ) ) {
+      elem.className = elem.className + ' ' + c;
+    }
+  };
+  removeClass = function( elem, c ) {
+    elem.className = elem.className.replace( classReg( c ), ' ' );
+  };
+}
+
+function toggleClass( elem, c ) {
+  var fn = hasClass( elem, c ) ? removeClass : addClass;
+  fn( elem, c );
+}
+
+var classie = {
+  // full names
+  hasClass: hasClass,
+  addClass: addClass,
+  removeClass: removeClass,
+  toggleClass: toggleClass,
+  // short names
+  has: hasClass,
+  add: addClass,
+  remove: removeClass,
+  toggle: toggleClass
+};
+
+// transport
+if ( typeof define === 'function' && define.amd ) {
+  // AMD
+  define( classie );
+} else if ( typeof exports === 'object' ) {
+  // CommonJS
+  module.exports = classie;
+} else {
+  // browser global
+  window.classie = classie;
+}
+
+})( window );
